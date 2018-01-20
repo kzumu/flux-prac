a8b5c86f881ad1c05d47b06a4f2103dd3cae3f84
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-01-20 15:44:45 +0900

    rename

---
+++ flux-prac/SignInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignInController.swift
+//  flux-prac
+//
+//  Created by 下村一将 on 2018/01/19.
+//  Copyright © 2018年 kazu. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
+
